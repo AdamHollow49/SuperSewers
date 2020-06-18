3c32e19a073823e6e0f5bd6304a5771ea0689460
AdamHollow49 <adamhollow49@gmail.com>
2020-06-18 22:19:24 +0100

    yes.

--- supersewers_dev/settings.py
+++ supersewers_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nlc+(w#7o-o1v5^13ko4v63&csybclktx=3)fg#nflzl#b437k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['supersewers.herokuapp.com']
 
 
 # Application definition
@@ -124,14 +124,10 @@ STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   'SuperSewersDev\staticfiles'
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
